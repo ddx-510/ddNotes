5ca0de54165be0baa54e43924e29164f38fee40b
ddx-510 <dai.tianle@dhs.sg>
2022-10-12 05:31:38 +0800

    Update production and dev diff

--- models/database.js
+++ models/database.js
@@ -5,14 +5,17 @@ const { response } = require("express");
 const Pool = require('pg').Pool
 
 const connectStr = process.env.DATABASE_URL;
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
+
 const isProduction = process.env.NODE_ENV === 'production'; // heroku default
+
 const currentDB = new Pool({
   // user: process.env.PG_USER,
   // host: process.env.PG_HOST,
   // database: process.env.PG_DATABASE,
   // password: process.env.PG_PASSWORD,
   // port: process.env.PG_PORT,
-  connectionString: connectStr,
+  connectionString: isProduction? connectStr : connectionString,
   ssl: isProduction
 });
 
