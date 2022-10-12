372483575d101ad2ce5178076c5cd0bb938dc09b
ddx-510 <dai.tianle@dhs.sg>
2022-10-12 19:27:57 +0800

    Include testing

--- models/database.js
+++ models/database.js
@@ -6,12 +6,23 @@ const Pool = require('pg').Pool
 
 const connectStr = process.env.DATABASE_URL;
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
-
+const testString = process.env.testURL;
 const isProduction = process.env.NODE_ENV === 'production'; // heroku default
+const isTesting = process.env.NODE_ENV === 'testing';
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+let currString;
+if (isProduction) {
+  currString = connectStr;
+} else if (isTesting) {
+  currString = testString;
+} else {
+  currString = connectionString
+}
 
 const currentDB = new Pool({
-  connectionString: isProduction ? connectStr : connectionString,
-  ssl: isProduction ? {rejectUnauthorized: false} : false 
+  connectionString: currString,
+  ssl: isDevelopment ? false : {rejectUnauthorized: false}
 });
 
 module.exports = currentDB;
